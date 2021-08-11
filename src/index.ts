b8ddcb2e044282146db2f10624d2f4b5daae37b5
hoersamu <samuelhoera@gmail.com>
2021-08-12 00:17:21 +0200

    feat: defined types for semantic and compilation stage

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,30 @@
-import {parser} from "./syntax.grammar"
-import {LezerLanguage, LanguageSupport, indentNodeProp, foldNodeProp, foldInside, delimitedIndent} from "@codemirror/language"
-import {styleTags, tags as t} from "@codemirror/highlight"
+import { styleTags, tags as t } from "@codemirror/highlight";
+import { LanguageSupport, LezerLanguage } from "@codemirror/language";
+import { CompilerResult } from "./compiler_types";
+import { parser } from "./syntax.grammar";
 
 export const karolLanguage = LezerLanguage.define({
-  parser: parser.configure({
-    props: [
-      styleTags({
-        Identifier: t.variableName,
-        Colour: t.literal,
-        Number: t.number,
-        comment: t.blockComment,
-        Keyword: t.keyword,
-        "( )": t.paren
-      })
-    ]
-  }),
-  languageData: {
-    commentTokens: {block: {open: "{", close: "}"}}
-  }
-})
+	parser: parser.configure({
+		props: [
+			styleTags({
+				Identifier: t.variableName,
+				Colour: t.literal,
+				Number: t.number,
+				comment: t.blockComment,
+				Keyword: t.keyword,
+				"( )": t.paren,
+			}),
+		],
+	}),
+	languageData: {
+		commentTokens: { block: { open: "{", close: "}" } },
+	},
+});
 
 export function karol() {
-  return new LanguageSupport(karolLanguage)
+	return new LanguageSupport(karolLanguage);
+}
+
+export function compile(): CompilerResult {
+	return { kind: "error", msg: "not implemented yet", line: 0 };
 }
