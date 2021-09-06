6c301bb5129fe41b0234d97e0447ef74f9aa60ef
Thomas Schuster <twihno@gmail.com>
2021-09-06 22:42:04 +0200

    reupload files

---
+++ src/compiler_types.ts
@@ -0,0 +1,48 @@
+export const callIdentifiers = new Set([
+	, "Schritt" 
+	, "LinksDrehen" 
+	, "RechtsDrehen" 
+	, "Hinlegen" 
+	, "Aufheben" 
+	, "MarkeSetzen" 
+	, "MarkeLöschen" 
+	, "Warten" 
+	, "Ton" 
+	, "Beenden" ]);
+
+export const conditionIdentifiers = new Set([
+	, "IstWand" 
+	, "NichtIstWand" 
+	, "IstZiegel"
+	, "NichtIstZiegel"
+	, "IstMarke"
+	, "NichtIstMarke" 
+	, "IstNorden" 
+	, "IstOsten" 
+	, "IstSüden" 
+	, "IstWesten" 
+	, "IstVoll" 
+	, "NichtIstVoll" 
+	, "IstLeer" 
+	, "NichtIstLeer" 
+	, "HatZiegel" ]);
+
+export type Position = {from: number; to: number};
+
+export type KarolError = { kind: "error"; msg: string; pos: Position };
+
+export type CompilationResult =
+	| { kind: "success"; result: GeneratorFunction }
+	| KarolError
+
+export type DefinitionCompilationResult =
+	| { kind: "success"; result: string; identifier: string }
+	| KarolError
+
+export type InnerCompilationResult =
+	| { kind: "success"; result: string }
+	| KarolError
+
+export type ExecutionResult =
+	| { kind: "success"; }
+	| KarolError
